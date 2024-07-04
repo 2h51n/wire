f3cfc0e6d2d3bddea19b78e68afa28e40b5e2db2
2h51n <lhi7247@gmail.com>
2024-07-04 11:56:06 +0900

    하단 완성, // 네비, 서브메뉴 전체 - 선택 기능 // modal 기능,   // 이미지 슬라이드 기능, jquery,

---
+++ js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  // submenu가 전체 보여지는 코드
+  // $(".navi > li").mouseover(function(){
+  //     $(".submenu").stop().slideDown(500)
+  // }).mouseout(function(){
+  //     $(".submenu").stop().slideUp(500)
+  // });
+  // navi에 해당하는 submenu가 나오는 코드
+//   navi - submenu 
+  $(".navi > li")
+    .mouseover(function () {
+      // console.log(this);
+      $(this).find(".submenu").stop().slideDown(500);
+    })
+    .mouseout(function () {
+      $(this).find(".submenu").stop().slideUp(500);
+    });
+  // 이미지 슬라이드 기능
+  setInterval(function () {
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginLeft: -1200 });
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginLeft: -2400 });
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginLeft: 0 });
+    $(".slidelist").delay(2000);
+  });
+  // tapmenu
+  $(function () {
+    $(".tapmenu >li >a").click(function () {
+      // console.log(this);
+      $(this).parent().addClass("active").siblings().removeClass("active");
+    });
+// modal 기능
+  });
+  $(".popup").click(function () {
+    $(".modal").addClass("active");
+  });
+//   modal close 기능
+  $(".close").click(function () {
+    $(".modal").removeClass("active");
+  });
+});
